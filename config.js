3996b3518a7682e0b2136dc5f472a762d80e4833
Fantox <shubhapratimbiswas@gmail.com>
2022-07-27 21:49:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,74 +1,37 @@
-//════════════════════════════//
-//If you want to recode, reupload
-//or copy the codes/script,
-//pls give credit
-//no credit? i will take action immediately
-//© 2022 Xeon Bot Inc. Cheems Bot MD
-//Thank you to Lord Buddha, Family and Myself
-//════════════════════════════//
-//recode kar ke youtube pe upload kar rhe ya
-//codes copy kar ke apne script me dal rhe
-//hai to, description me xeon ka yt channel
-// ka link paste kr dena as a cradit or github 
-//repo me bhi tag kardena baki jo
-//bhi karna hai apki marzi, thank you!🦄
-//════════════════════════════//
-//If you recode and uploading on your channel
-//or copy pasting the codes in ur script, 
-//i give permission to do as long as you
-//put Xeons youtube channel link in the video
-//description and tag me on githuh repo, 
-//thank you🦄
-//════════════════════════════//
-
 const fs = require("fs")
 const chalk = require("chalk")
 
-//aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = true //auto reading in gc (true to on, false to off)
-global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
-global.available = true //auto available (true to on, false to off)
+global.available = true
+global.autoReadGc = true
+global.autoReadAll = false
+global.antitags = true
 
-//documents variants
-global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
-global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-global.doc4 = 'application/zip'
-global.doc5 = 'application/pdf'
-global.doc6 = 'application/vnd.android.package-archive'
 
-//owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.Owner = ['918101187835']
+global.OwnerNumber = ['918101187835']
+global.ownertag = ['918101187835']
+global.BotName = "Miku"
+global.packname = "Miku bot"
+global.author = "By: Fantox"
+global.OwnerName = "Fantox"
+global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
+global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.sessionName = "session"
+global.prefa = ['-']
+global.location = "West Bengal, India"
+global.reactmoji = "❤️"
+global.themeemoji = "💖"
+global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
+global.websitex = "https://github.com/FantoX001"
+global.lolhuman = "KaysaS"
 
-//bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
-global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
-global.themeemoji = "🐶"
-global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
-global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
 
-//Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
 global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
-//database
+
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -88,26 +51,25 @@ global.gcrevoke = []
 global.autorep = []
 global.ntilink = []
 
-//omther
-global.lolhuman = "KaysaS"
-global.sessionName = "session"
-global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+
 global.mess = {
-    success: 'Done✓',
-    admin: 'This feature is only for admins!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This feature is only for owner',
-    group: 'This feature is only for groups!',
-    private: 'This feature is only for private chats!',
-    bot: 'This feature is only for bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
+    jobdone: 'Job done...',
+    useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+    botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
+    botowner: 'Only my *Owner* can use this command, Baka!',
+    grouponly: 'This command is only made for *Groups*, Baka!',
+    privateonly: 'This command is only made for *Private Chat*, Baka!',
+    botonly: 'Only the *Bot itself* can use this command!',
+    waiting: 'Chotto Matte...',
+    nolink: 'Please provide me *link*, Baka!',
+    error: 'An error occurd!',
+    banned: 'You are *Banned* fron using commands!',
+    bangc: 'This Group is *Banned* from using Commands!',
+    nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+    
 }
+
+
 global.limitawal = {
     premium: "Infinity",
     free: 2,
@@ -125,16 +87,16 @@ global.limitawal = {
     rakyat: "Infinity",
     free: 100
 }
-// api website by xenz
+
+
 global.APIs = {
 	zenz: 'https://zenzapis.xyz',
 }
-
-// key
 global.APIKeys = {
-	'https://zenzapis.xyz': 'Your Key',
+	'https://zenzapis.xyz': '5d1197db351b',
 }
-//menu logo maker
+
+
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
