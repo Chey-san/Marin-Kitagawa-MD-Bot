76de6a616a66491ab9f6032ae87a680a5947cb30
Fantox <shubhapratimbiswas@gmail.com>
2022-07-31 11:31:12 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -1559,7 +1559,7 @@ case 'nsfwmenu':
     if (isBan) return reply(mess.banned)	 			
     if (isBanChat) return reply(mess.bangc)
     if (!AntiNsfw) return reply(mess.nonsfw)
-        reply(`    *―――――〈  NSFW Menu  〉―――――*\n\nhentaivideo, blowjobgif, hneko, masturbation, thighs, pussy, panties, orgy, ahegao, ass, bdsm, blowjob, cuckold, ero, gasm, cum, femdom, foot, gangbang, glasses, jahy, trap, blowjobgif, spank, hneko, hwaifu, gasm`)
+        reply(` *━━━━━━〈  NSFW Menu  〉━━━━━━*\n\n🏮 hentaivideo, blowjobgif, hneko, masturbation, thighs, pussy, panties, orgy, ahegao, ass, bdsm, blowjob, cuckold, ero, gasm, cum, femdom, foot, gangbang, glasses, jahy, trap, blowjobgif, spank, hneko, hwaifu, gasm`)
     break
 
     
@@ -4703,77 +4703,77 @@ case 'help': case 'h': case 'menu': case 'allmenu': case 'listmenu':{
     if (isBan) return reply(mess.banned)	 			
     if (isBanChat) return reply(mess.bangc)
       
- const helpmenu = `Konichiwa *${pushname}* dear,
+ const helpmenu = `Konichiwa *${pushname}* Senpai,
 
 I am *Miku Nakano*, a bot developed by Fantox.
 
-My prefix is:  ${prefix}
+🍁 My prefix is:  ${prefix}
 
 Here's the list of my Commands.
  
 
 
-    *―――――〈  Core  〉―――――*
+ *━━━━━━〈  Core  〉━━━━━━*
 
-profile, exp, leaderboard, help, delete, listgc, listpc, welcome, speedtest, ping, support, repo, script
+🏮 profile, exp, leaderboard, help, delete, listgc, listpc, welcome, speedtest, ping, support, repo, script 
  
-    *―――――〈  Owner  〉―――――*
+ *━━━━━━〈  Owner  〉━━━━━━*
 
-self, public, ban, bangroup, bye, join, bye, block, unblock, broadcast
+🏮 self, public, ban, bangroup, bye, join, bye, block, unblock, broadcast 
 
-    *―――――〈  Group  〉―――――*
+ *━━━━━━〈  Group  〉━━━━━━*
 
-promote, demote, revoke, remove, tagall, hidetag, groupsetting, grouplink, setgcpp, setname, setdesc, group, nsfw 
+🏮 promote, demote, revoke, remove, tagall, hidetag, groupsetting, grouplink, setgcpp, setname, setdesc, group, nsfw 
 
-    *―――――〈  Anti Link  〉―――――*
+ *━━━━━━〈  Anti Link  〉━━━━━━*
  
-antilinkgc, antilinktg, antilinktt, antilinkytch, antilinkytvid, antilinkig, antilinkfb, antilinktwit, antilinkall, antiwame
+🏮 antilinkgc, antilinktg, antilinktt, antilinkytch, antilinkytvid, antilinkig, antilinkfb, antilinktwit, antilinkall, antiwame
 
-    *―――――〈  Search  〉―――――*
+ *━━━━━━〈  Search  〉━━━━━━*
 
-play, song, yts, lyrics, google, playstore, gimage, pinterest, image, movie, wallpaper, searchgc, happymod, wikimedia, ringtone, anime, animestory, manga, ringtone   
+🏮 play, song, yts, lyrics, google, playstore, gimage, pinterest, image, movie, wallpaper, searchgc, happymod, wikimedia, ringtone, anime, animestory, manga, ringtone   
 
-    *―――――〈  Convert  〉―――――*
+ *━━━━━━〈  Convert  〉━━━━━━*
 
-sticker, toimg, tovideo, togif , steal, stickermeme, emojimix, tourl, tomp3, toaudio
+🏮 sticker, toimg, tovideo, togif , steal, stickermeme, emojimix, tourl, tomp3, toaudio
 
-    *―――――〈  Audio  〉―――――*
+ *━━━━━━〈  Audio  〉━━━━━━*
 
-bass, tempo, blown, deep, earrape, fast, fat, nightcore, reverse, robot, slow, squirrel
+🏮 bass, tempo, blown, deep, earrape, fast, fat, nightcore, reverse, robot, slow, squirrel
 
-    *―――――〈  Reactions  〉―――――*
+ *━━━━━━〈  Reactions  〉━━━━━━*
 
-bonk, cry, bully, cuddle, hug, kiss, lick, pat, smug, yeet, blush, smile, wave, highfive, handhold, nom, glomp, bite, slap, kill, happy, wink, poke, dance, cringe
+🏮 bonk, cry, bully, cuddle, hug, kiss, lick, pat, smug, yeet, blush, smile, wave, highfive, handhold, nom, glomp, bite, slap, kill, happy, wink, poke, dance, cringe
 
-    *―――――〈  Downloader  〉―――――*
+ *━━━━━━〈  Downloader  〉━━━━━━*
 
-play, ytmp3, ytmp4, ytvideo, mediafire, instagram, igtv, facebook, fbmp3, twitter, twittermp3, tiktok, tiktokaudio, tiktoknowm, mediafire  
+🏮 play, ytmp3, ytmp4, ytvideo, mediafire, instagram, igtv, facebook, fbmp3, twitter, twittermp3, tiktok, tiktokaudio, tiktoknowm, mediafire  
 
-    *―――――〈  Weeb  〉―――――*
+ *━━━━━━〈  Weeb  〉━━━━━━*
 
-waifu, loli, neko, ppcouple, feed, foxgirl, feed, meow, tickle, wallpaper, coffee, animenom, waifu3, neko2, feed, meow, tickle, migumin, awoo, animewallpaper2, anime, manga
+🏮 waifu, loli, neko, ppcouple, feed, foxgirl, feed, meow, tickle, wallpaper, coffee, animenom, waifu3, neko2, feed, meow, tickle, migumin, awoo, animewallpaper2, anime, manga
 
-    *―――――〈  Informative  〉―――――*
+ *━━━━━━〈  Informative  〉━━━━━━*
 
-animequote, quote, covid, earthquake, wiki
+🏮 animequote, quote, covid, earthquake, wiki
 
-    *―――――〈  Others  〉―――――*
+ *━━━━━━〈  Others  〉━━━━━━*
 
-image, imginvert, imagestupid, wasted, jail, stickermeme, quotes, darkjoke, 
+🏮 image, imginvert, imagestupid, wasted, jail, stickermeme, quotes, darkjoke 🏮
 
-    *―――――〈  Fun  〉―――――*
+ *━━━━━━〈  Fun  〉━━━━━━*
 
-truth, dare, couple, soulmate, handsomecheck, beautifulcheck, awesomecheck, greatcheck, gaycheck, cutecheck, lesbiancheck, hornycheck, prettycheck, lovelycheck, uglycheck, charactercheck
+🏮 truth, dare, couple, soulmate, handsomecheck, beautifulcheck, awesomecheck, greatcheck, gaycheck, cutecheck, lesbiancheck, hornycheck, prettycheck, lovelycheck, uglycheck, charactercheck
 
-    *―――――〈  Essentials  〉―――――*
+ *━━━━━━〈  Essentials  〉━━━━━━*
 
-translate, fliptext, toletter
+🏮 translate, fliptext, toletter
 
-    *―――――〈  NSFW  〉―――――*
+ *━━━━━━〈  NSFW  〉━━━━━━*
 
-Type " *${prefix}nsfw* " then enable NSFW (Admin only!) 
+🍁 Type " *${prefix}nsfw* " then enable NSFW (Admin only!) 
 
-Then type " *${prefix}nsfwmenu* " to get full list of NSFW commands.
+🍁 Then type " *${prefix}nsfwmenu* " to get full list of NSFW commands.
 
 
 
@@ -4782,12 +4782,12 @@ Then type " *${prefix}nsfwmenu* " to get full list of NSFW commands.
  『  *${global.BotName}*  』
  Powered by: *Fantox*
 
- To use any of these commands type 
+ 🍁 To use any of these commands type 
  " *${prefix}<Command name>* ".
  
- To get Support Group link type " *${prefix}support* ".
+ 🍁 To get Support Group link type " *${prefix}support* ".
 
- Type " *${prefix}help* " to get full command list.`
+ 🍁 Type " *${prefix}help* " to get full command list.`
      
  let buttonshelpm = [
     {buttonId: `-owner`, buttonText: {displayText: 'Bot Owner'}, type: 1}
