881b31b14bfe9f0a73e8e1ad452d72cb071f95bb
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-08-04 03:19:58 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -4854,7 +4854,7 @@ case '':
                 {buttonId: `-help`, buttonText: {displayText: 'Help'}, type: 1}
                 ]
                 let buttonMessage = {
-                    file: Miku.sendMessage(m.chat,{video:fs.readFileSync('./system/marin.mp4'),gifPlayback:true,caption:needhelpmenu},{quoted:m}),
+              systu.sendMessage(m.chat,{video:fs.readFileSync('./system/miku2.mp4','miku.mp4','marin.mp4')ifPlayback:true,caption:needhelpmenu},{quoted:m}),
                     caption: needhelpmenu,
                     footer: `${global.BotName}`,
                     buttons: butRun,
